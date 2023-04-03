1a922adbaed0aee50a9b82cc1366fa39bab069ff
EC2 Default User <125885895+taichi328@users.noreply.github.com>
2023-04-03 15:46:01 +0000

    [update]bookページのレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,12 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to new_book_path
   end
 
   def index
@@ -15,9 +15,10 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.new(book_params)
   end
-  
-  
+
+
    private
 
   def book_params
