104826e3f45d57c13b632f0640449a8767ade2fd
EC2 Default User <125885895+taichi328@users.noreply.github.com>
2023-04-04 11:49:54 +0000

    [add]indexの右側、userのeditページのレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
   def show
-    @user =　User.find(params[:id])
+    @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to books_path(current_user.id)
   end
 end
